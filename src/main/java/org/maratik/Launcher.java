562281e710762d4e3fc317c1da4acf8f152027af
Marat Buharov <marat.buharov@gmail.com>
2017-04-22 21:06:10 +0300

    Add some debug

--- src/main/java/org/maratik/Launcher.java
+++ src/main/java/org/maratik/Launcher.java
@@ -8,12 +8,16 @@ import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.util.thread.ThreadPool;
 
+import javax.annotation.Nonnull;
+
 /**
  * Created by maratik.
  */
 public class Launcher {
     private static final Logger LOG = LogManager.getLogger(Launcher.class);
     private static final int HTTP_PORT = 8080;
+    private static final int MAX_THREADS = 100;
+    private static final int MIN_THREADS = 10;
 
     public static void main(String[] args) {
         LOG.debug("Start main");
@@ -26,24 +30,27 @@ public class Launcher {
     }
 
     private void startServer() throws Exception {
+        LOG.debug("Initializing server");
         Server server = new Server(createThreadPool());
         server.setConnectors(new Connector[] { createConnector(HTTP_PORT, server) });
+        LOG.debug("Start server");
         server.start();
+        LOG.debug("Waiting to finish server thread");
         server.join();
-
-        Thread mainThread = Thread.currentThread();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            server.stop();
-        });
+        LOG.debug("Exit from main thread");
     }
 
-    private Connector createConnector(int httpPort, Server server) {
+    @Nonnull
+    private Connector createConnector(int httpPort, @Nonnull Server server) {
+        LOG.debug("Creating server connector for port = {}", httpPort);
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(httpPort);
         return connector;
     }
 
+    @Nonnull
     private ThreadPool createThreadPool() {
-        return new QueuedThreadPool(100, 10);
+        LOG.debug("Creating thread pool with max_threads = {} and min_threads = {}", MAX_THREADS, MIN_THREADS);
+        return new QueuedThreadPool(MAX_THREADS, MIN_THREADS);
     }
 }
