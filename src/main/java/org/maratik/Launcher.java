f2148b01f94e78ef2a4468a6a2f0f95d452dcaa9
Marat Bukharov <maratik@yandex-team.ru>
2017-04-22 12:54:37 +0300

    Add logger

---
+++ src/main/java/org/maratik/Launcher.java
@@ -0,0 +1,15 @@
+package org.maratik;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by maratik.
+ */
+public class Launcher {
+    private static final Logger LOG = LogManager.getLogger(Launcher.class);
+
+    public static void main(String[] args) {
+        LOG.debug("Start main");
+    }
+}
